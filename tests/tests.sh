fd3ead4f42f4c48e03a2234b8bcec622614f1603
Anton Shapka <Anton.Shapka@accellion.com>
2015-02-09 21:54:53 +0200

    Fixed mistake

--- tests/tests.sh
+++ tests/tests.sh
@@ -14,7 +14,6 @@ testNothing() {
 }
 
 #All of these does not work with GIT v1.7.1 on Linux
-
 testGitAttributesLf() {
     echo $'* text eol=lf\n*.txt text\n' > "${0%/*}/../.gitattributes"
 
@@ -26,6 +25,7 @@ testGitAttributesLf() {
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 
+#All of these does not work with GIT v1.7.1 on Linux
 testGitAttributesAuto() {
     echo $'* text auto\n*.txt text\n' > "${0%/*}/../.gitattributes"
 
@@ -37,10 +37,8 @@ testGitAttributesAuto() {
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 
-# It does not work with OSX and GIT v2.2.2 and v1.9.3
 testGitConfigEol() {
-    echo $'[core]\n\tcore.eol = lf\n' > "${0%/*}/../.gitconfig"
-
+    git config core.eol lf && \
     git add -A && git commit -m 'test' &> /dev/null && git push origin test &> /dev/null && \
     git clone $(git config remote.origin.url) tmprepo &> /dev/null && cd tmprepo && \
     git checkout -b test origin/test &> /dev/null && cd ..
@@ -49,48 +47,43 @@ testGitConfigEol() {
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 
-# It does not work with OSX and GIT v2.2.2 and v1.9.3
 testGitConfigInput() {
-    echo $'[core]\n\tcore.autocrlf = input\n' > "${0%/*}/../.gitconfig"
-
+    git config core.autocrlf input && \
     git add -A && git commit -m 'test' &> /dev/null && git push origin test &> /dev/null && \
     git clone $(git config remote.origin.url) tmprepo &> /dev/null && cd tmprepo && \
     git checkout -b test origin/test &> /dev/null && cd ..
-    assertEquals "This file has CRLF end lines" 1 `file tmprepo/crlf.txt | grep -c "CRLF"`
+    assertEquals "This file has CRLF end lines" 0 `file tmprepo/crlf.txt | grep -c "CRLF"`
     assertEquals "This file has CR end lines" 1 `file tmprepo/cr.txt | grep -c " CR "`
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 
-# It does not work with OSX and GIT v2.2.2 and v1.9.3
 testGitConfigTrue() {
-    echo $'[core]\n\tcore.autocrlf = true\n' > "${0%/*}/../.gitconfig"
-
+    git config core.autocrlf true && \
     git add -A && git commit -m 'test' &> /dev/null && git push origin test &> /dev/null && \
     git clone $(git config remote.origin.url) tmprepo &> /dev/null && cd tmprepo && \
     git checkout -b test origin/test &> /dev/null && cd ..
-    assertEquals "This file has CRLF end lines" 1 `file tmprepo/crlf.txt | grep -c "CRLF"`
+    assertEquals "This file has CRLF end lines" 0 `file tmprepo/crlf.txt | grep -c "CRLF"`
     assertEquals "This file has CR end lines" 1 `file tmprepo/cr.txt | grep -c " CR "`
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 
-# It does not work with OSX and GIT v2.2.2 and v1.9.3
 testGitConfigTrueAndSafe() {
-    echo $'[core]\n\tcore.autocrlf = true\n\tcore.safeocrlf = true\n' > "${0%/*}/../.gitconfig"
-
+    git config core.autocrlf true && \
+    git config core.safeocrlf true && \
     git add -A && git commit -m 'test' &> /dev/null && git push origin test &> /dev/null && \
     git clone $(git config remote.origin.url) tmprepo &> /dev/null && cd tmprepo && \
     git checkout -b test origin/test &> /dev/null && cd ..
-    assertEquals "This file has CRLF end lines" 1 `file tmprepo/crlf.txt | grep -c "CRLF"`
+    assertEquals "This file has CRLF end lines" 0 `file tmprepo/crlf.txt | grep -c "CRLF"`
     assertEquals "This file has CR end lines" 1 `file tmprepo/cr.txt | grep -c " CR "`
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
 testGitConfigInputAndSafe() {
-    echo $'[core]\n\tcore.autocrlf = input\n\tcore.safeocrlf = true\n' > "${0%/*}/../.gitconfig"
-
+    git config core.autocrlf input && \
+    git config core.safeocrlf true && \
     git add -A && git commit -m 'test' &> /dev/null && git push origin test &> /dev/null && \
     git clone $(git config remote.origin.url) tmprepo &> /dev/null && cd tmprepo && \
     git checkout -b test origin/test &> /dev/null && cd ..
-    assertEquals "This file has CRLF end lines" 1 `file tmprepo/crlf.txt | grep -c "CRLF"`
+    assertEquals "This file has CRLF end lines" 0 `file tmprepo/crlf.txt | grep -c "CRLF"`
     assertEquals "This file has CR end lines" 1 `file tmprepo/cr.txt | grep -c " CR "`
     assertEquals "This file does not have other then LF end lines" 0 `file tmprepo/lf.txt | grep -c "terminators"`
 }
